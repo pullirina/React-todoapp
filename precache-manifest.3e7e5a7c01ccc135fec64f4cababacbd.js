5bb8fab8cd8fb49fe8fff024df5e4e54e56e4ea8
Amith Raul <amith.raul@autodesk.com>
2020-03-02 17:27:16 -0800

    Updates

---
+++ precache-manifest.3e7e5a7c01ccc135fec64f4cababacbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52eee898897f71a8cd6b0ccc86a6ce7",
+    "url": "/React-todoapp/index.html"
+  },
+  {
+    "revision": "c2a7640b588ea0425e5e",
+    "url": "/React-todoapp/static/css/main.7b057e18.chunk.css"
+  },
+  {
+    "revision": "3a4f491feac7b6eac32f",
+    "url": "/React-todoapp/static/js/2.8a584521.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-todoapp/static/js/2.8a584521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a7640b588ea0425e5e",
+    "url": "/React-todoapp/static/js/main.350e49fa.chunk.js"
+  },
+  {
+    "revision": "846052fc37a6af1304f1",
+    "url": "/React-todoapp/static/js/runtime-main.4e127f31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
